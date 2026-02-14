6002735194666121a63c6890fcff6fba0c907124
Sige Wang <wangfiz@bc.edu>
2026-02-14 17:14:32 -0500

    Initial Commit

---
+++ WordGarden/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WordGarden
+//
+//  Created by Wang Sige on 2/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Wang Sige on 2/14/26.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
